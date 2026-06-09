3aeb3ce40ad017cc84241bb874e1db8d9ca3f009
winzfs <jazzhjm@gmail.com>
2026-06-09 13:47:23 +0900

    use polling interval constants for messages

--- apps/web/src/features/home/api/useMessagePolling.ts
+++ apps/web/src/features/home/api/useMessagePolling.ts
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 import { loadD1ChatMessages, type D1ChatMessage } from './d1ChatMessages';
+import { POLLING_INTERVALS } from './pollingIntervals';
 
 export function useMessagePolling(roomId: string, onUpdate: (messages: D1ChatMessage[]) => void) {
   useEffect(() => {
     const timer = window.setInterval(() => {
       if (document.hidden) return;
       loadD1ChatMessages(roomId).then(onUpdate);
-    }, 3000);
+    }, POLLING_INTERVALS.chatMessages);
 
     return () => window.clearInterval(timer);
   }, [onUpdate, roomId]);
