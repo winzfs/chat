6740324005c7782281ee3cbb03e0960945e6b610
winzfs <jazzhjm@gmail.com>
2026-06-09 10:13:41 +0900

    wrap app with signup gate

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SignupGate } from './features/auth/SignupGate';
 import { HomeScreenNext } from './features/home/HomeScreenNext';
 import './shared/styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HomeScreenNext />
+    <SignupGate>
+      <HomeScreenNext />
+    </SignupGate>
   </React.StrictMode>,
 );
