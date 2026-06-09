38bb57fbe9f9369918af04dd33860624f71fd624
winzfs <jazzhjm@gmail.com>
2026-06-10 07:37:41 +0900

    rename app to flirting

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.chitchat.app',
-  appName: 'ChitChat',
+  appId: 'com.flirting.app',
+  appName: '플러팅',
   webDir: 'apps/web/dist',
   server: {
     androidScheme: 'https',
