fee0883cca3126a032ffc91910eb26b964f7cdcb
winzfs <jazzhjm@gmail.com>
2026-06-09 16:45:27 +0900

    add api cors middleware for native app

---
+++ functions/api/_middleware.ts
@@ -0,0 +1,25 @@
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, x-profile-id',
+  'Access-Control-Max-Age': '86400',
+};
+
+export const onRequest: PagesFunction = async (context) => {
+  if (context.request.method === 'OPTIONS') {
+    return new Response(null, { status: 204, headers: corsHeaders });
+  }
+
+  const response = await context.next();
+  const headers = new Headers(response.headers);
+
+  for (const [key, value] of Object.entries(corsHeaders)) {
+    headers.set(key, value);
+  }
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers,
+  });
+};
