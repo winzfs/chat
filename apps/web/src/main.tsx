90f4f9798bcd6d3618bc7361387f1b5eac3dcf56
winzfs <jazzhjm@gmail.com>
2026-06-10 08:13:20 +0900

    show launch splash in native app

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { SignupGate } from './features/auth/SignupGate';
 import { HomeScreenNext } from './features/home/HomeScreenNext';
+import { AppLaunchSplash } from './shared/components/AppLaunchSplash';
 import './shared/styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <SignupGate>
       <HomeScreenNext />
+      <AppLaunchSplash />
     </SignupGate>
   </React.StrictMode>,
 );
