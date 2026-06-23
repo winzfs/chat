2f67634545291386d154d93b25bc4cea35d5f3d2
winzfs <jazzhjm@gmail.com>
2026-06-23 20:05:33 +0900

    feat: wrap the app in a root error boundary

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { AuthSessionGate } from './features/auth/AuthSessionGate';
 import { SignupGate } from './features/auth/SignupGate';
 import { HomeScreenNext } from './features/home/HomeScreenNext';
+import { AppErrorBoundary } from './shared/components/AppErrorBoundary';
 import { AppLaunchSplash } from './shared/components/AppLaunchSplash';
 import './shared/styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AuthSessionGate>
-      <SignupGate>
-        <HomeScreenNext />
-      </SignupGate>
-    </AuthSessionGate>
-    <AppLaunchSplash />
+    <AppErrorBoundary>
+      <AuthSessionGate>
+        <SignupGate>
+          <HomeScreenNext />
+        </SignupGate>
+      </AuthSessionGate>
+      <AppLaunchSplash />
+    </AppErrorBoundary>
   </React.StrictMode>,
 );
